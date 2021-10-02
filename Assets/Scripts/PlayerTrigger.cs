4f655066a00b413f2b2df280e3cd03ab3edd815a
Jaoel <josv.svensson@gmail.com>
2021-10-02 14:54:08 +0200

    Add player trigger class

---
+++ Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace LD49 {
+    public class PlayerTrigger : MonoBehaviour {
+        public delegate void PlayerTriggerDelegate();
+        public event PlayerTriggerDelegate PlayerEnter;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.TryGetComponent(out Player player)) {
+                PlayerEnter?.Invoke();
+            }
+        }
+    }
+}
