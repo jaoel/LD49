8fece06b452414ab085d2d4e9fbdaaf554432498
Jaoel <josv.svensson@gmail.com>
2021-10-02 14:54:14 +0200

    Add level class

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace LD49 {
+    public class Level : MonoBehaviour {
+        public Transform startPosition;
+        public PlayerTrigger endTrigger;
+
+        private void OnEnable() {
+            endTrigger.PlayerEnter += OnPlayerTriggerEnd;
+        }
+
+        private void OnDisable() {
+            endTrigger.PlayerEnter -= OnPlayerTriggerEnd;
+        }
+
+        private void Start() {
+            Player player = FindObjectOfType<Player>();
+            if (player != null) {
+                player.transform.position = startPosition.position;
+                player.transform.eulerAngles = new Vector3(0f, startPosition.eulerAngles.y, 0f);
+            }
+        }
+
+        private void OnPlayerTriggerEnd() {
+            Debug.Log("You Win!");
+        }
+    }
+}
