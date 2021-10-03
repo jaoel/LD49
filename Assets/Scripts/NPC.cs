e50bbd0b236c6091313ddd3a2e3ed857124156da
Jaoel <josv.svensson@gmail.com>
2021-10-03 19:48:43 +0200

    Add npc framework

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace LD49 {
+    public class NPC : MonoBehaviour {
+        private enum AnimationType {
+            IdleCoffee,
+            IdleWaiting,
+            IdleNormal,
+            SittingIdle,
+        }
+
+        [SerializeField]
+        private AnimationType animation = AnimationType.IdleNormal;
+
+        [SerializeField]
+        private Animator animator;
+
+        private AnimationType currentAnimation;
+
+        private void Awake() {
+            foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>()) {
+                rb.isKinematic = true;
+            }
+            foreach (Collider c in GetComponentsInChildren<Collider>()) {
+                c.enabled = false;
+            }
+        }
+
+        private void Start() {
+            UpdateAnimationIndex();
+        }
+
+        private void UpdateAnimationIndex() {
+            currentAnimation = animation;
+            animator.Play(currentAnimation.ToString());
+        }
+
+        private void Update() {
+            if (currentAnimation != animation) {
+                UpdateAnimationIndex();
+            }
+        }
+
+        public void SetRagdoll(Vector3 explosionPosition, float explosionForce) {
+            foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>()) {
+                rb.isKinematic = false;
+                rb.AddExplosionForce(explosionForce, explosionPosition, 5f);
+            }
+            foreach (Collider c in GetComponentsInChildren<Collider>()) {
+                c.enabled = true;
+            }
+        }
+    }
+}
