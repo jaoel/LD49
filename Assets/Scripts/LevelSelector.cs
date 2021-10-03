467c3b00339a82fbfc8fe97b73b96ec62f694c19
Jaoel <josv.svensson@gmail.com>
2021-10-04 00:21:41 +0200

    Add poop animation

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 namespace LD49 {
     public class LevelSelector : MonoBehaviour {
@@ -9,6 +10,12 @@ namespace LD49 {
             int hasCompleted = PlayerPrefs.GetInt($"level{index}", 0);
             if (hasCompleted == 0) {
                 poopGameObject.SetActive(false);
+            } else if (hasCompleted == 1) {
+                //PlayerPrefs.SetInt($"level{index}", 2);
+                Vector3 targetScale = poopGameObject.transform.localScale;
+                Debug.Log(targetScale);
+                poopGameObject.transform.localScale = Vector3.zero;
+                poopGameObject.transform.DOScale(targetScale, 1f).SetDelay(index * 0.5f).SetEase(Ease.OutElastic);
             }
         }
     }
