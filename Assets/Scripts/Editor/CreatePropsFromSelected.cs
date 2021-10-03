dead222c06dd1e70f96d0de9b3724ec3bdc9a36a
Jaoel <josv.svensson@gmail.com>
2021-10-03 11:42:07 +0200

    Add prop script

---
+++ Assets/Scripts/Editor/CreatePropsFromSelected.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace LD49.Editor {
+    public class CreatePropsFromSelected {
+        [MenuItem("Tools/Make Props")]
+        static void DoSomething() {
+            foreach (GameObject selected in Selection.gameObjects) {
+                if (Selection.activeObject is GameObject gameObject) {
+                    CreateOrReplacePrefab(selected);
+                }
+            } 
+        }
+
+        private static void CreateOrReplacePrefab(GameObject toCopy) {
+            Material defaultMaterial = AssetDatabase.LoadAssetAtPath<Material>("Assets/Materials/UnlitPalette.mat");
+            GameObject copy = GameObject.Instantiate(toCopy);
+            copy.name = toCopy.name;
+            copy.transform.position = Vector3.zero;
+            copy.transform.rotation = Quaternion.identity;
+
+            Rigidbody rigidbody = copy.AddComponent<Rigidbody>();
+            MeshCollider meshCollider = copy.AddComponent<MeshCollider>();
+            meshCollider.convex = true;
+            Prop prop = copy.AddComponent<Prop>();
+
+            foreach (var renderer in copy.GetComponentsInChildren<Renderer>()) {
+                renderer.sharedMaterial = defaultMaterial;
+            }
+
+            PrefabUtility.SaveAsPrefabAssetAndConnect(copy, GetPath(copy), InteractionMode.UserAction, out bool success);
+            if (!success) {
+                Debug.LogError($"Could not create prefab for game object {copy.name}");
+                return;
+            }
+        }
+
+        private static string GetPath(GameObject copy) {
+            return $"Assets/Prefabs/Props/{copy.name}.prefab";
+        }
+    }
+}
---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace LD49 {
+    public class Prop : MonoBehaviour {
+        public float score = 10f;
+    }
+}
