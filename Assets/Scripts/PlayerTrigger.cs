646b8506ed0e24be5a9d9fc957f41b35b8012daa
Bentebent <henriksson.tim@gmail.com>
2021-10-03 23:07:04 +0200

    asdasd

--- Assets/Scripts/PlayerTrigger.cs
+++ Assets/Scripts/PlayerTrigger.cs
@@ -5,9 +5,25 @@ namespace LD49 {
         public delegate void PlayerTriggerDelegate();
         public event PlayerTriggerDelegate PlayerEnter;
 
+        [SerializeField]
+        private GameObject marker = null;
+
         private void OnTriggerEnter(Collider other) {
             if (other.TryGetComponent(out Player player)) {
                 PlayerEnter?.Invoke();
+            }
+        }
+
+        private void Update() {
+            if (PlayerEnter != null && marker != null && !marker.activeInHierarchy) {
+                marker.SetActive(true);
+            }
+            else if (PlayerEnter == null) {
+                marker.SetActive(false);
+            }
+
+            if (marker.activeSelf) {
+
             }
         }
     }
