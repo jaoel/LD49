ca21f8d6b17820d1f60e0a88e2ba249811d45045
Jaoel <josv.svensson@gmail.com>
2021-10-03 14:34:35 +0200

    Add explosions when clicking in menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -12,5 +12,21 @@ namespace LD49 {
             LevelManager.RequestLevel(0);
             SceneManager.LoadScene("MainScene");
         }
+
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Plane plane = new Plane(Vector3.up, 0f);
+                if (plane.Raycast(mouseRay, out float enter)) {
+                    Vector3 pos = mouseRay.origin + mouseRay.direction * enter;
+
+                    foreach (Rigidbody rb in FindObjectsOfType<Rigidbody>()) {
+                        if (Vector3.Distance(rb.position, pos) < 5f) {
+                            rb.AddExplosionForce(500f, pos, 5f);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
