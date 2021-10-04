efb1815fd04e05c3f2dcac50a09adc61d1f21911
Bentebent <henriksson.tim@gmail.com>
2021-10-04 21:31:17 +0200

    apsduasoiud

--- Assets/Scripts/PlayerTrigger.cs
+++ Assets/Scripts/PlayerTrigger.cs
@@ -22,7 +22,7 @@ namespace LD49 {
         }
 
         private void OnTriggerEnter(Collider other) {
-            if (other.TryGetComponent(out Player player)) {
+            if (other.TryGetComponent(out Player player) && !GameManager.Instance.IsDead) {
                 PlayerEnter?.Invoke();
                 playerEnterUnityEvent?.Invoke();
             }
