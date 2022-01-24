22c7f9e713dfdfac6431a4830ef398ffb7fb79b9
Jaoel <josv.svensson@gmail.com>
2022-01-24 23:34:42 +0100

    Add fart sound event to farticle system

--- Assets/Scripts/FarticleSystem.cs
+++ Assets/Scripts/FarticleSystem.cs
@@ -2,12 +2,22 @@
 
 namespace LD49 {
     public class FarticleSystem : MonoBehaviour {
+        public FMODUnity.EventReference fartEvent = new FMODUnity.EventReference();
+
         public ParticleSystem[] particleSystems;
+
+        private void Awake() {
+            if (fartEvent.IsNull) {
+                fartEvent = FMODUnity.RuntimeManager.PathToEventReference("event:/Farts/Fart_Default");
+            }
+        }
 
         public void Fart() {
             foreach (var ps in particleSystems) {
                 ps.Play();
             }
+
+            FMODUnity.RuntimeManager.PlayOneShotAttached(fartEvent.Guid, gameObject);
         }
     }
 }
